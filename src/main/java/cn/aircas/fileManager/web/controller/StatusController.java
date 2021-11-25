f6aab6886ac387e4688f8e11dc79015f1be4a17b
Vanishrain <vanishrain@163.com>
2021-11-25 09:50:17 +0800

    新增： 1. 添加获取服务版本接口

--- src/main/java/cn/aircas/fileManager/web/controller/StatusController.java
+++ src/main/java/cn/aircas/fileManager/web/controller/StatusController.java
@@ -2,9 +2,11 @@ package cn.aircas.fileManager.web.controller;
 
 
 import cn.aircas.fileManager.commons.entity.common.CommonResult;
+import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StatusController {
 
-    @GetMapping()
-    @ApiOperation("服务")
-    public CommonResult<String> status() {
+    @Value("${value.app.version}")
+    private String version;
 
+    @Value("${value.app.build.time}")
+    private String buildTime;
+
+    @GetMapping()
+    @ApiOperation("获取服务状态")
+    public CommonResult<String> status() {
         return new CommonResult<String>().data("文件服务运行正常").success().message("文件服务运行正常");
+    }
+
+    @GetMapping("/version")
+    @ApiOperation("获取服务版本号")
+    public CommonResult<JSONObject> version(){
+        JSONObject result = new JSONObject();
+        result.put("version",version);
+        result.put("buildTime",buildTime);
+        return new CommonResult<JSONObject>().data(result).success().message("文件服务运行正常");
     }
 }
