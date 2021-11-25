44493988fc047560d63c04a0e019e9113205584d
Vanishrain <vanishrain@163.com>
2021-11-25 09:50:17 +0800

    修改： 1. 使用springboot的buildProperties属性获取程序构建时间

--- src/main/java/cn/aircas/fileManager/web/controller/StatusController.java
+++ src/main/java/cn/aircas/fileManager/web/controller/StatusController.java
@@ -2,9 +2,13 @@ package cn.aircas.fileManager.web.controller;
 
 
 import cn.aircas.fileManager.commons.entity.common.CommonResult;
+import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StatusController {
 
-    @GetMapping()
-    @ApiOperation("服务")
-    public CommonResult<String> status() {
+    @Autowired
+    BuildProperties buildProperties;
 
+    @GetMapping()
+    @ApiOperation("获取服务状态")
+    public CommonResult<String> status() {
         return new CommonResult<String>().data("文件服务运行正常").success().message("文件服务运行正常");
+    }
+
+    @GetMapping("/version")
+    @ApiOperation("获取服务版本号")
+    public CommonResult<JSONObject> version(){
+        JSONObject result = new JSONObject();
+        result.put("version",this.buildProperties.getVersion());
+        result.put("buildTime",this.buildProperties.getArtifact());
+        return new CommonResult<JSONObject>().data(result).success().message("服务运行正常");
     }
 }
