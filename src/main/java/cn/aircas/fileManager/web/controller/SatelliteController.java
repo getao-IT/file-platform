749ba7b8985f82bd5169ab3b85794b7c6851e6f9
DESKTOP-J0MBT8K\dell <asd>
2022-04-14 17:19:29 +0800

    更改字体 ，返回值类型

--- src/main/java/cn/aircas/fileManager/web/controller/SatelliteController.java
+++ src/main/java/cn/aircas/fileManager/web/controller/SatelliteController.java
@@ -15,7 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@Api(tags = "衛星信息管理接口")
+@Api(tags = "卫星信息管理接口")
 @RequestMapping("/satellite")
 @Slf4j
 public class SatelliteController {
@@ -23,79 +23,79 @@ public class SatelliteController {
     @Autowired
     private IFileSatelliteInfoService satelliteInfoService;
 
-    @Log(value = "查詢所有的衛星列表")
+    @Log(value = "查询所有的卫星列表")
     //@OperationLog(value = "分页查询影像信息")
     @GetMapping("/list")
-    @ApiOperation("查詢所有的衛星列表")
-    public CommonResult querySatelliteList(){
-        return new CommonResult().success().data(satelliteInfoService.querySatelliteList());
+    @ApiOperation("查询所有的卫星列表")
+    public CommonResult<List<FileSatelliteInfo>> querySatelliteList(){
+        return new CommonResult<List<FileSatelliteInfo>>().success().data(satelliteInfoService.querySatelliteList());
     }
 
-    @Log(value = "查詢相關傳感器的衛星列表")
+    @Log(value = "查询相关传感器的卫星列表")
     //@OperationLog(value = "分页查询影像信息")
     @GetMapping("/listBySensor")
-    @ApiOperation("查詢相關傳感器的衛星列表")
-    public CommonResult querySatelliteList(Long[] sensorIds){
+    @ApiOperation("查询相关传感器的卫星列表")
+    public CommonResult<List<FileSatelliteInfo>> querySatelliteList(Long[] sensorIds){
         if (sensorIds==null || sensorIds.length==0){
-            return new CommonResult().fail().message("傳感器id不可爲空");
+            return new CommonResult<List<FileSatelliteInfo>>().fail().message("传感器id不可为空");
         }
-        return new CommonResult().success().data(satelliteInfoService.querySatelliteListBySensor(Arrays.asList(sensorIds)));
+        return new CommonResult<List<FileSatelliteInfo>>().success().data(satelliteInfoService.querySatelliteListBySensor(Arrays.asList(sensorIds)));
     }
 
 
-    @Log(value = "添加衛星")
+    @Log(value = "添加卫星")
     //@OperationLog(value = "分页查询影像信息")
     @PostMapping
-    @ApiOperation("查詢相關傳感器的衛星列表")
-    public CommonResult add(@RequestBody SatelliteParam param){
+    @ApiOperation("查询相关传感器的卫星列表")
+    public CommonResult<Boolean> add(@RequestBody SatelliteParam param){
         FileSatelliteInfo satelliteInfo = new FileSatelliteInfo();
         satelliteInfo.setSatelliteNo(param.getSatelliteNo());
         satelliteInfo.setSatelliteName(param.getSatelliteName());
         satelliteInfo.setCreateTime(new Date());
         satelliteInfo.setUpdateTime(new Date());
-        return new CommonResult().success().data(satelliteInfoService.saveSatellite(satelliteInfo));
+        return new CommonResult<Boolean>().success().data(satelliteInfoService.saveSatellite(satelliteInfo));
     }
 
-    @Log(value = "添加完整的衛星信息")
+    @Log(value = "添加完整的卫星信息")
     //@OperationLog(value = "分页查询影像信息")
     @PostMapping("/addFull")
-    @ApiOperation("添加完整的衛星信息")
-    public CommonResult addFull(@RequestBody SatelliteFullParam param){
+    @ApiOperation("添加完整的卫星信息")
+    public CommonResult<Boolean> addFull(@RequestBody SatelliteFullParam param){
         FileSatelliteInfo satelliteInfo = new FileSatelliteInfo();
         satelliteInfo.setSatelliteNo(param.getSatelliteNo());
         satelliteInfo.setSatelliteName(param.getSatelliteName());
         satelliteInfo.setCreateTime(new Date());
         satelliteInfo.setUpdateTime(new Date());
-        return new CommonResult().success().data(satelliteInfoService.saveFullSatellite(satelliteInfo,param.getSensorList()));
+        return new CommonResult<Boolean>().success().data(satelliteInfoService.saveFullSatellite(satelliteInfo,param.getSensorList()));
     }
 
 
-    @Log(value = "添加給衛星添加傳感器關係信息")
+    @Log(value = "添加给卫星添加传感器关系信息")
     //@OperationLog(value = "分页查询影像信息")
     @PostMapping("/addsensorRelation")
-    @ApiOperation("添加完整的衛星信息")
-    public CommonResult addSensorRelation(@RequestBody SatelliteSensorRelationParam param){
+    @ApiOperation("添加完整的卫星信息")
+    public CommonResult<Boolean> addSensorRelation(@RequestBody SatelliteSensorRelationParam param){
         FileSatelliteSensorRelation relation = new FileSatelliteSensorRelation();
         relation.setSensorId(param.getSensorId());
         relation.setSatelliteId(param.getSatelliteId());
         relation.setCreateTime(new Date());
         relation.setUpdateTime(new Date());
-        return satelliteInfoService.saveRelation(relation)?new CommonResult().success():new CommonResult().fail();
+        return satelliteInfoService.saveRelation(relation)?new CommonResult<Boolean>().success():new CommonResult<Boolean>().fail();
     }
 
-    @Log(value = "更新衛星信息")
+    @Log(value = "更新卫星信息")
     //@OperationLog(value = "分页查询影像信息")
     @PutMapping
-    @ApiOperation("查詢相關傳感器的衛星列表")
-    public CommonResult update(@RequestBody SatelliteParam param){
+    @ApiOperation("查询相关传感器的卫星列表")
+    public CommonResult<Boolean> update(@RequestBody SatelliteParam param){
         if (param.getId() ==null){
-            return new CommonResult().fail().message("缺少衛星id信息");
+            return new CommonResult<Boolean>().fail().message("缺少卫星id信息");
         }
         FileSatelliteInfo satelliteInfo = new FileSatelliteInfo();
         satelliteInfo.setId(param.getId());
         satelliteInfo.setSatelliteNo(param.getSatelliteNo());
         satelliteInfo.setSatelliteName(param.getSatelliteName());
         satelliteInfo.setUpdateTime(new Date());
-        return new CommonResult().success().data(satelliteInfoService.updateSatellite(satelliteInfo));
+        return new CommonResult<Boolean>().success().data(satelliteInfoService.updateSatellite(satelliteInfo));
     }
 }
--- src/main/java/cn/aircas/fileManager/web/controller/SensorController.java
+++ src/main/java/cn/aircas/fileManager/web/controller/SensorController.java
@@ -17,7 +17,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@Api(tags = "傳感器信息管理接口")
+@Api(tags = "传感器信息管理接口")
 @RequestMapping("/sensor")
 @Slf4j
 public class SensorController {
@@ -25,52 +25,52 @@ public class SensorController {
     @Autowired
     private IFileSatelliteSensorInfoService sensorInfoService;
 
-    @Log(value = "查詢所有的傳感器列表")
+    @Log(value = "查询所有的传感器列表")
     //@OperationLog(value = "分页查询影像信息")
     @GetMapping("/list")
-    @ApiOperation("查詢所有的衛星列表")
-    public CommonResult querySatelliteList(){
-        return new CommonResult().success().data(sensorInfoService.querySensorList());
+    @ApiOperation("查询所有的卫星列表")
+    public CommonResult<List<FileSatelliteSensorInfo>> querySatelliteList(){
+        return new CommonResult<List<FileSatelliteSensorInfo>>().success().data(sensorInfoService.querySensorList());
     }
 
-    @Log(value = "查詢相關傳感器的衛星列表")
+    @Log(value = "查询相关传感器的卫星列表")
     //@OperationLog(value = "分页查询影像信息")
     @GetMapping("/listBySatellite")
-    @ApiOperation("查詢相關傳感器的衛星列表")
-    public CommonResult querySatelliteList(Long[] satelliteIds){
+    @ApiOperation("查询相关传感器的卫星列表")
+    public CommonResult<List<FileSatelliteSensorInfo>> querySatelliteList(Long[] satelliteIds){
         if (satelliteIds==null || satelliteIds.length ==0){
-            return new CommonResult().fail().message("衛星id不可爲空");
+            return new CommonResult<List<FileSatelliteSensorInfo>>().fail().message("卫星id不可为空");
         }
-        return new CommonResult().success().data(sensorInfoService.querySensorListBySatelliteIds(Arrays.asList(satelliteIds)));
+        return new CommonResult<List<FileSatelliteSensorInfo>>().success().data(sensorInfoService.querySensorListBySatelliteIds(Arrays.asList(satelliteIds)));
     }
 
 
-    @Log(value = "添加傳感器")
+    @Log(value = "添加传感器")
     //@OperationLog(value = "分页查询影像信息")
     @PostMapping
-    @ApiOperation("添加傳感器")
-    public CommonResult add(@RequestBody SensorParam param){
+    @ApiOperation("添加传感器")
+    public CommonResult<Boolean> add(@RequestBody SensorParam param){
         FileSatelliteSensorInfo sensorInfo = new FileSatelliteSensorInfo();
         sensorInfo.setSensorNo(param.getSensorNo());
         sensorInfo.setSensorName(param.getSensorName());
         sensorInfo.setCreateTime(new Date());
         sensorInfo.setUpdateTime(new Date());
-        return new CommonResult().success().data(sensorInfoService.saveSensor(sensorInfo));
+        return new CommonResult<Boolean>().success().data(sensorInfoService.saveSensor(sensorInfo));
     }
 
-    @Log(value = "更新傳感器信息")
+    @Log(value = "更新传感器信息")
     //@OperationLog(value = "分页查询影像信息")
     @PutMapping
-    @ApiOperation("更新傳感器信息")
-    public CommonResult update(@RequestBody SensorParam param){
+    @ApiOperation("更新传感器信息")
+    public CommonResult<Boolean> update(@RequestBody SensorParam param){
         if (param.getId() ==null){
-            return new CommonResult().fail().message("缺少傳感器id信息");
+            return new CommonResult<Boolean>().fail().message("缺少传感器id信息");
         }
         FileSatelliteSensorInfo sensorInfo = new FileSatelliteSensorInfo();
         sensorInfo.setId(param.getId());
         sensorInfo.setSensorNo(param.getSensorNo());
         sensorInfo.setSensorName(param.getSensorName());
         sensorInfo.setUpdateTime(new Date());
-        return new CommonResult().success().data(sensorInfoService.updateSensor(sensorInfo));
+        return new CommonResult<Boolean>().success().data(sensorInfoService.updateSensor(sensorInfo));
     }
 }
