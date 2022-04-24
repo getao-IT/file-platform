9292f22c4feb5d33e3ec9657608ea8a57619542b
guo <guo@gmail.com>
2022-04-24 18:52:13 +0800

    update

--- src/main/java/cn/aircas/fileManager/web/service/impl/LabServiceImpl.java
+++ src/main/java/cn/aircas/fileManager/web/service/impl/LabServiceImpl.java
@@ -118,8 +118,11 @@ public class LabServiceImpl implements LabService {
         List<Image> imageList = this.imageMapper.selectBatchIds(messageList);
         List<JSONObject> result =  new ArrayList<>();
         for (Integer id : messageList) {
-            Image image = imageList.stream().filter(imageTemp->imageTemp.getId()==id).findFirst().get();
-            result.add(JSONObject.parseObject(JSONObject.toJSONString(image)));
+            for (Image ob :imageList) {
+                if (ob.getId() == id){
+                    result.add(JSONObject.parseObject(JSONObject.toJSONString(ob)));
+                }
+            }
         }
 //        List<Image> imageList = this.imageMapper.listImageByMessage(messageList);
 //        List<JSONObject> result = imageList.stream().map(JSONObject::toJSONString).map(JSONObject::parseObject).collect(Collectors.toList());
@@ -160,8 +163,11 @@ public class LabServiceImpl implements LabService {
         List<Image> imageList = this.imageMapper.selectBatchIds(messageList);
         List<JSONObject> result =  new ArrayList<>();
         for (Integer id : messageList) {
-            Image image = imageList.stream().filter(imageTemp->imageTemp.getId()==id).findFirst().get();
-            result.add(JSONObject.parseObject(JSONObject.toJSONString(image)));
+            for (Image ob :imageList) {
+                if (ob.getId() == id){
+                    result.add(JSONObject.parseObject(JSONObject.toJSONString(ob)));
+                }
+            }
         }
         //List<JSONObject> result  = this.imageFileService.listFileInfosByIds(messageList,false);
 //        List<Image> imageList = this.imageMapper.listImageByMessage(messageList);
